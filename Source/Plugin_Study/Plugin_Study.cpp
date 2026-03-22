f9532793434dac278935ed71acb54bda6032ac60
ShineSky <1058131716@qq.com>
2026-03-22 20:14:39 +0800

    精简文件

--- Source/Plugin_Study/Plugin_Study.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Plugin_Study.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plugin_Study, "Plugin_Study" );
-
-DEFINE_LOG_CATEGORY(LogPlugin_Study)
- 
--- Source/Plugin_Study/Plugin_StudyGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Plugin_StudyGameMode.h"
-
-APlugin_StudyGameMode::APlugin_StudyGameMode()
-{
-	// stub
-}
